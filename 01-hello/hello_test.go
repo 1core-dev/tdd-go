e2316ea170fe3a7509b57a94ca2d036a601c1eb8
1core-dev <1core@proton.me>
2026-05-01 15:36:42 +0300

    Empty arg case

--- 01-hello/hello_test.go
+++ 01-hello/hello_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Homer")
-	want := "Hello, Homer"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Homer")
+		want := "Hello, Homer"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("sating 'Hello World' when no args provided", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
 }
