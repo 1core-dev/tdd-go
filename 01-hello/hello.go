ccbeb3be8d7e9973f8f398ab7b41cd4c23576b31
1core-dev <1core@proton.me>
2026-05-01 15:29:15 +0300

    Hello with name arg

--- 01-hello/hello.go
+++ 01-hello/hello.go
@@ -1,9 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
-	Hello()
+	fmt.Println(Hello("world"))
 }
 
-func Hello() string {
-	return "Hello, world"
+func Hello(name string) string {
+	return "Hello, " + name
 }
--- 01-hello/hello_test.go
+++ 01-hello/hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	got := Hello("Homer")
+	want := "Hello, Homer"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
