2ae28ba3a251bf8d5489408edfbd3b578247c16e
1core-dev <1core@proton.me>
2026-05-03 13:13:15 +0300

    Sum of slices implementation

--- 04-collection/sum_test.go
+++ 04-collection/sum_test.go
@@ -1,6 +1,9 @@
 package collection
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of any numbers", func(t *testing.T) {
@@ -13,4 +16,13 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d", got, want)
 		}
 	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{3, 7}, []int{7, 13})
+	want := []int{10, 20}
+
+	if !slices.Equal(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
