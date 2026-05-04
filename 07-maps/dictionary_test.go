8974c71fb6176b834b193a7b34f5444a5e4a96a1
1core-dev <1core@proton.me>
2026-05-04 17:36:50 +0300

    Known and unknown test cases

--- 07-maps/dictionary_test.go
+++ 07-maps/dictionary_test.go
@@ -7,10 +7,19 @@ import (
 func TestSearch(t *testing.T) {
 	dict := Dictionary{"test": "test value"}
 
-	got := dict.Search("test")
-	want := "test value"
+	t.Run("known word", func(t *testing.T) {
+		got := dict.Search("test")
+		want := "test value"
 
-	assertString(t, got, want)
+		assertString(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		got := dict.Search("unknown")
+		want := ""
+
+		assertString(t, got, want)
+	})
 }
 
 func assertString(t testing.TB, got, want string) {
