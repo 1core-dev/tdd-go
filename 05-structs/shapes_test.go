b3593cfc27b033e03dfdc0f5ff22d7186e64d8a1
1core-dev <1core@proton.me>
2026-05-04 10:57:27 +0300

    Test area with table-tests

--- 05-structs/shapes_test.go
+++ 05-structs/shapes_test.go
@@ -18,22 +18,20 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
+	areaTests := []struct {
+		area Shape
+		want float64
+	}{
+		{Rectangle{8.0, 10.0}, 80.0},
+		{Circle{10.0}, 314.1592653589793},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.area.Area()
+		want := tt.want
+
 		if got != want {
 			t.Errorf("got %g want %g", got, want)
 		}
 	}
-
-	t.Run("rectangle area", func(t *testing.T) {
-		rectangle := Rectangle{8.0, 10.0}
-		checkArea(t, rectangle, 80.0)
-	})
-
-	t.Run("circle area", func(t *testing.T) {
-		circle := Circle{10.0}
-		checkArea(t, circle, 314.1592653589793)
-	})
-
 }
