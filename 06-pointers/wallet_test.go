d89c35984bc86cc01018fc94fe39504076e48ae5
1core-dev <1core@proton.me>
2026-05-04 14:57:52 +0300

    Refactoring with helper func

--- 06-pointers/wallet_test.go
+++ 06-pointers/wallet_test.go
@@ -5,28 +5,25 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	t.Run("deposit", func(t *testing.T) {
-		wallet := Wallet{}
-
-		wallet.Deposit(10)
-
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		t.Helper()
 		got := wallet.Balance()
-		want := Bitcoin(10)
 
 		if got != want {
 			t.Errorf("got %s want %s", got, want)
 		}
+
+	}
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+
+		wallet.Deposit(10)
+		assertBalance(t, wallet, 10)
 	})
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: 20}
 
 		wallet.Withdraw(10)
-
-		got := wallet.Balance()
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, 10)
 	})
 }
