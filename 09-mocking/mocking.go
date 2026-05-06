ddafdb0c70f70db6e6882b58a3cdd6ea7cb1549b
1core-dev <1core@proton.me>
2026-05-06 14:02:37 +0300

    Refactor with sleep timeout

--- 09-mocking/mocking.go
+++ 09-mocking/mocking.go
@@ -4,13 +4,22 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
+)
+
+const (
+	countdownStart = 3
+	finalWord      = "Go!"
 )
 
 func Countdown(w io.Writer) {
-	for i := 3; i > 0; i-- {
+	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
 		fmt.Fprintln(w, i)
 	}
-	fmt.Fprint(w, "Go!")
+
+	time.Sleep(1 * time.Second)
+	fmt.Fprint(w, finalWord)
 }
 
 func main() {
