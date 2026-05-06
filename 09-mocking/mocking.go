6d9d665af792a1b8cc94824ab9732cd4fa8781d1
1core-dev <1core@proton.me>
2026-05-06 13:41:39 +0300

    Dummy countdown

---
+++ 09-mocking/mocking.go
@@ -0,0 +1,15 @@
+package mocking
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func Countdown(w io.Writer) {
+	fmt.Fprint(w, "3")
+}
+
+func main() {
+	Countdown(os.Stdout)
+}
---
+++ 09-mocking/mocking_test.go
@@ -0,0 +1,19 @@
+package mocking
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestCountdown(t *testing.T) {
+	buf := bytes.Buffer{}
+
+	Countdown(&buf)
+
+	got := buf.String()
+	want := "3"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
