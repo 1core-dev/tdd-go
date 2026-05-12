3b599a5e556c1d40696f2ff8fe2154ee30ce0c56
1core-dev <1core@proton.me>
2026-05-12 17:16:32 +0300

    WebChecker concurrency implementation

--- 10-concurrency/webchecker_test.go
+++ 10-concurrency/webchecker_test.go
@@ -3,10 +3,27 @@ package concurrency
 import (
 	"maps"
 	"testing"
+	"time"
 )
 
 func mockWebChecker(url string) bool {
 	return url != "waat://invalid.site"
+}
+
+func slowWebCheckerStub(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := range urls {
+		urls[i] = "new url"
+	}
+
+	for b.Loop() {
+		CheckWebsites(slowWebCheckerStub, urls)
+	}
 }
 
 func TestCheckWebsites(t *testing.T) {
