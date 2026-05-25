093366b1b562129ef0546de7459f8cc3c804980e
1core-dev <1core@proton.me>
2026-05-25 17:32:48 +0300

    Refactor to use table tests

--- 12-reflection/reflection_test.go
+++ 12-reflection/reflection_test.go
@@ -1,25 +1,35 @@
 package reflection
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestWalk(t *testing.T) {
-	expected := "Chris"
-	var got []string
 
-	x := struct {
-		Name string
-	}{expected}
-
-	walk(x, func(input string) {
-		got = append(got, input)
-	})
-
-	if len(got) != 1 {
-		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
+	cases := []struct {
+		Name          string
+		Input         any
+		ExpectedCalls []string
+	}{
+		{
+			"struct with one field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
 	}
-	if got[0] != expected {
-		t.Errorf("got %q, want %q", got[0], expected)
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(s string) {
+				got = append(got, s)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
 	}
 }
